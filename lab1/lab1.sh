a7e3eb88c0ab5fbc8e8789d9b118f5cb1cb05f68
OMWi <bsuiromwi@gmail.com>
2022-04-13 15:16:27 +0300

    lab1 small fix

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -1,8 +1,8 @@
 file_name="stats.txt"
 echo "Current session stats:" > ${file_name}
 echo "username = `whoami`" >> ${file_name}
-echo "time = `date '+%H:%M:%S'`" >> ${file_name}
-echo "date = `date '+%d.%m.%Y'`" >> ${file_name}
+echo "time = `date "+%H:%M:%S"`" >> ${file_name}
+echo "date = `date "+%d.%m.%Y"`" >> ${file_name}
 echo "working directory = '`pwd`'" >> ${file_name}
 echo "proccesses number = `ps -e | wc -l`" >> ${file_name}
 echo "uptime = `uptime -p` since `uptime -s`" >> ${file_name}
